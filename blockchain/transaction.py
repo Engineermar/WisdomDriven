d5635d0a9af1978b1cf491fb163806e5c28219c1
LLyons <elchicoblk@msn.com>
2018-11-20 02:47:18 +0000

    Update transaction.py

--- blockchain/transaction.py
+++ blockchain/transaction.py
@@ -1,7 +1,17 @@
 import random
 from wdapp.models import OrderStatus
 random.seed(0)
+import hashlib, json, sys
 
+def hashMe(msg=""):
+    # For convenience, this is a helper function that wraps our hashing algorithm
+    if type(msg)!=str:
+        msg = json.dumps(msg,sort_keys=True)  # If we don't sort keys, we can't guarantee repeatability!
+        
+    if sys.version_info.major == 2:
+        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
+    else:
+        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
 def makeTransaction(maxValue=3):
     # This will create valid transactions in the range of (1,maxValue)
     sign      = int(random.getrandbits(1))*2 - 1   # This will randomly choose -1 or 1
