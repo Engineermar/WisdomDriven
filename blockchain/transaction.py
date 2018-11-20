663a62120aa6bed5b296767594a18c2605e8b04e
LLyons <elchicoblk@msn.com>
2018-11-20 02:45:45 +0000

    Update transaction.py

--- blockchain/transaction.py
+++ blockchain/transaction.py
@@ -1,27 +1,17 @@
-import hashlib, json, sys
 import random
-def hashMe(msg=""):
-    # For convenience, this is a helper function that wraps our hashing algorithm
-    if type(msg)!=str:
-        msg = json.dumps(msg,sort_keys=True)  # If we don't sort keys, we can't guarantee repeatability!
-        
-    if sys.version_info.major == 2:
-        return unicode(hashlib.sha256(msg).hexdigest(),'utf-8')
-    else:
-        return hashlib.sha256(str(msg).encode('utf-8')).hexdigest()
-
-
+from wdapp.models import OrderStatus
 random.seed(0)
 
 def makeTransaction(maxValue=3):
     # This will create valid transactions in the range of (1,maxValue)
     sign      = int(random.getrandbits(1))*2 - 1   # This will randomly choose -1 or 1
     amount    = random.randint(1,maxValue)
-    alicePays = sign * amount
-    bobPays   = -1 * alicePays
+    driver_received = sign * amount
+    business_received   = -1 * driver_received
     # By construction, this will always return transactions that respect the conservation of tokens.
     # However, note that we have not done anything to check whether these overdraft an account
-    return {u'Alice':alicePays,u'Bob':bobPays}
+    return {u'Driver':Driver_received,u'Business':Business_received}
+
 
 #txnBuffer = [makeTransaction() for i in range(30)]
 
